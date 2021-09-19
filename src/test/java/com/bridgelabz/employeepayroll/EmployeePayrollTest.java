251069f5157cbaf29b9e6dcee385103b522fe685
rajshetye11 <rajshetye11@gmail.com>
2021-09-19 21:44:58 +0530

    [raj] : added ability to update data in database

--- src/test/java/com/bridgelabz/employeepayroll/EmployeePayrollTest.java
+++ src/test/java/com/bridgelabz/employeepayroll/EmployeePayrollTest.java
@@ -15,19 +15,28 @@ public class EmployeePayrollTest {
 	}
 	
 	 @Test
-	    public void get_AllEmployee() throws SQLException {
-	    	Connection dbConnection= new EmployeePayroll().establishDatabaseConnection();
-	    	Statement st =  dbConnection.createStatement();
-	        PreparedStatement ps= dbConnection.prepareStatement("select * from employee");
-	        ResultSet resultset= ps.executeQuery();
-	        while (resultset.next())
-	        {
-	            System.out.println(
-	                    resultset.getString(1)+" "+
-	                            resultset.getString(2)+ " "+
-	                            resultset.getString(3)+" "+
-	                            resultset.getString(4)
-	            );
-	        }
+	 public void get_AllEmployee() throws SQLException {
+	    Connection dbConnection= new EmployeePayroll().establishDatabaseConnection();
+	    Statement st =  dbConnection.createStatement();
+	    PreparedStatement ps= dbConnection.prepareStatement("select * from employee_payroll");
+	    ResultSet resultset= ps.executeQuery();
+	    while (resultset.next())
+	    {
+	        System.out.println(
+	        		resultset.getString(1)+" "+
+	                resultset.getString(2)+ " "+
+	                resultset.getString(3)+" "+
+	                resultset.getString(4)
+	        );
+	    }
+	 }
+	 
+	 @Test
+	 public void updateEmployee() throws SQLException {
+	    Connection dbConnection= new EmployeePayroll().establishDatabaseConnection();
+	    Statement st =  dbConnection.createStatement();
+	    PreparedStatement ps= dbConnection.prepareStatement("update employee_payroll set salary=300000.00 where id = 2");
+	    int resultset= ps.executeUpdate();
+	   
 	 }
 }
